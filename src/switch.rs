2fa2327b47c7b2d63d115c31cb0cedb6dc0a0314
sawyer bristol <sawyerbristol@gmail.com>
2024-12-02 15:29:51 -0700

    got usb descriptor macro

--- src/switch.rs
+++ src/switch.rs
@@ -11,6 +11,11 @@ use embassy_usb::{
 };
 use embassy_usb::{Builder, Config};
 
+mod hid_descriptor;
+use hid_descriptor::SwitchProControllerReport;
+use serde::Serialize;
+use usbd_hid::descriptor::SerializedDescriptor;
+
 pub struct HidEndpoints<'d, D: Driver<'d>> {
     writer: HidWriter<'d, D, 64>,
     reader: HidReader<'d, D, 64>,
@@ -29,7 +34,7 @@ impl<'d, D: Driver<'d>> HidEndpoints<'d, D> {
         drop(func);
 
         let config = hid::Config {
-            report_descriptor: &HID_DESCRIPTOR,
+            report_descriptor: &SwitchProControllerReport::desc(),
             request_handler: None,
             poll_ms: 0x08,
             max_packet_size: 64,
@@ -74,27 +79,12 @@ impl<'d, D: Driver<'d>> HidEndpoints<'d, D> {
         }
 
         if self.handshake {
-            // let report = ProControllerReport {
-            //     button: Button::SWITCH_A,
-            //     DPAD: Dpad::DPAD_TOP,
-            //     LX: 0,
-            //     LY: 0,
-            //     RX: 0,
-            //     RY: 0,
-            //     VendorSpec: 0,
-            // };
-            // match self.writer.write_serialize(&report).await {
-            //     Ok(()) => {}
-            //     Err(e) => warn!("Failed to send report: {:?}", e),
-            // }
-            unwrap!(
-                self.writer
-                    .write(&[
-                        0x19, 0x01, 0x03, 0x07, 0x00, 0x00, 0x92, 0x00, 0x00, 0x00, 0x00, 0x00,
-                        0x01,
-                    ])
-                    .await
-            );
+            let mut report = SwitchProControllerReport::new();
+            report.press_button(hid_descriptor::Button::A);
+            match self.writer.write_serialize(&report).await {
+                Ok(()) => {}
+                Err(e) => warn!("Failed to send report: {:?}", e),
+            }
             info!("sent button, again");
         }
     }
@@ -186,149 +176,96 @@ impl RequestHandler for UsbRequestHandler {
     }
 }
 
-#[derive(serde::Serialize)]
-pub enum Button {
-    SWITCH_Y = 0x01,
-    SWITCH_B = 0x02,
-    SWITCH_A = 0x04,
-    SWITCH_X = 0x08,
-    SWITCH_L = 0x10,
-    SWITCH_R = 0x20,
-    SWITCH_ZL = 0x40,
-    SWITCH_ZR = 0x80,
-    SWITCH_MINUS = 0x100,
-    SWITCH_PLUS = 0x200,
-    SWITCH_LCLICK = 0x400,
-    SWITCH_RCLICK = 0x800,
-    SWITCH_HOME = 0x1000,
-    SWITCH_CAPTURE = 0x2000,
-}
-
-#[derive(serde::Serialize)]
-pub enum Dpad {
-    DPAD_TOP = 0x00,
-    DPAD_TOP_RIGHT = 0x01,
-    DPAD_RIGHT = 0x02,
-    DPAD_BOTTOM_RIGHT = 0x03,
-    DPAD_BOTTOM = 0x04,
-    DPAD_BOTTOM_LEFT = 0x05,
-    DPAD_LEFT = 0x06,
-    DPAD_TOP_LEFT = 0x07,
-    DPAD_CENTER = 0x08,
-}
-
-const STICK_MIN: u8 = 0;
-const STICK_CENTER: u8 = 128;
-const STICK_MAX: u8 = 255;
-
-#[derive(serde::Serialize)]
-pub struct ProControllerReport {
-    pub button: Button,
-    pub DPAD: Dpad,
-    pub LX: u8, // Left  Stick X
-    pub LY: u8, // Left  Stick Y
-    pub RX: u8, // Right Stick X
-    pub RY: u8, // Right Stick Y
-    pub VendorSpec: u8,
-}
-
-impl usbd_hid::descriptor::AsInputReport for ProControllerReport {}
-
 pub static HID_DESCRIPTOR: &'static [u8] = &[
-    // HID Descriptor
-    0x05, 0x01, // Usage Page (Generic Desktop Ctrls)
-    0x15, 0x00, // infoical Minimum (0)
-    0x09, 0x04, // Usage (Joystick)
-    0xA1, 0x01, // Collection (Application)
-    0x85, 0x30, //   Report ID (48)
-    0x05, 0x01, //   Usage Page (Generic Desktop Ctrls)
-    0x05, 0x09, //   Usage Page (Button)
-    0x19, 0x01, //   Usage Minimum (0x01)
-    0x29, 0x0A, //   Usage Maximum (0x0A)
-    0x15, 0x00, //   infoical Minimum (0)
-    0x25, 0x01, //   infoical Maximum (1)
-    0x75, 0x01, //   Report Size (1)
-    0x95, 0x0A, //   Report Count (10)
-    0x55, 0x00, //   Unit Exponent (0)
-    0x65, 0x00, //   Unit (None)
-    0x81, 0x02, //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x05, 0x09, //   Usage Page (Button)
-    0x19, 0x0B, //   Usage Minimum (0x0B)
-    0x29, 0x0E, //   Usage Maximum (0x0E)
-    0x15, 0x00, //   infoical Minimum (0)
-    0x25, 0x01, //   infoical Maximum (1)
-    0x75, 0x01, //   Report Size (1)
-    0x95, 0x04, //   Report Count (4)
-    0x81, 0x02, //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x75, 0x01, //   Report Size (1)
-    0x95, 0x02, //   Report Count (2)
-    0x81, 0x03, //   Input (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x0B, 0x01, 0x00, 0x01, 0x00, //   Usage (0x010001)
-    0xA1, 0x00, //   Collection (Physical)
-    0x0B, 0x30, 0x00, 0x01, 0x00, //     Usage (0x010030)
-    0x0B, 0x31, 0x00, 0x01, 0x00, //     Usage (0x010031)
-    0x0B, 0x32, 0x00, 0x01, 0x00, //     Usage (0x010032)
-    0x0B, 0x35, 0x00, 0x01, 0x00, //     Usage (0x010035)
-    0x15, 0x00, //     infoical Minimum (0)
-    0x27, 0xFF, 0xFF, 0x00, 0x00, //     infoical Maximum (65534)
-    0x75, 0x10, //     Report Size (16)
-    0x95, 0x04, //     Report Count (4)
-    0x81, 0x02, //     Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0xC0, //   End Collection
-    0x0B, 0x39, 0x00, 0x01, 0x00, //   Usage (0x010039)
-    0x15, 0x00, //   infoical Minimum (0)
-    0x25, 0x07, //   infoical Maximum (7)
-    0x35, 0x00, //   Physical Minimum (0)
-    0x46, 0x3B, 0x01, //   Physical Maximum (315)
-    0x65, 0x14, //   Unit (System: English Rotation, Length: Centimeter)
-    0x75, 0x04, //   Report Size (4)
-    0x95, 0x01, //   Report Count (1)
-    0x81, 0x02, //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x05, 0x09, //   Usage Page (Button)
-    0x19, 0x0F, //   Usage Minimum (0x0F)
-    0x29, 0x12, //   Usage Maximum (0x12)
-    0x15, 0x00, //   infoical Minimum (0)
-    0x25, 0x01, //   infoical Maximum (1)
-    0x75, 0x01, //   Report Size (1)
-    0x95, 0x04, //   Report Count (4)
-    0x81, 0x02, //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x75, 0x08, //   Report Size (8)
-    0x95, 0x34, //   Report Count (52)
-    0x81, 0x03, //   Input (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x06, 0x00, 0xFF, //   Usage Page (Vendor Defined 0xFF00)
-    0x85, 0x21, //   Report ID (33)
-    0x09, 0x01, //   Usage (0x01)
-    0x75, 0x08, //   Report Size (8)
-    0x95, 0x3F, //   Report Count (63)
-    0x81, 0x03, //   Input (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x85, 0x81, //   Report ID (-127)
-    0x09, 0x02, //   Usage (0x02)
-    0x75, 0x08, //   Report Size (8)
-    0x95, 0x3F, //   Report Count (63)
-    0x81, 0x03, //   Input (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x85, 0x01, //   Report ID (1)
-    0x09, 0x03, //   Usage (0x03)
-    0x75, 0x08, //   Report Size (8)
-    0x95, 0x3F, //   Report Count (63)
-    0x91,
-    0x83, //   Output (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Volatile)
-    0x85, 0x10, //   Report ID (16)
-    0x09, 0x04, //   Usage (0x04)
-    0x75, 0x08, //   Report Size (8)
-    0x95, 0x3F, //   Report Count (63)
-    0x91,
-    0x83, //   Output (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Volatile)
-    0x85, 0x80, //   Report ID (-128)
-    0x09, 0x05, //   Usage (0x05)
-    0x75, 0x08, //   Report Size (8)
-    0x95, 0x3F, //   Report Count (63)
-    0x91,
-    0x83, //   Output (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Volatile)
-    0x85, 0x82, //   Report ID (-126)
-    0x09, 0x06, //   Usage (0x06)
-    0x75, 0x08, //   Report Size (8)
-    0x95, 0x3F, //   Report Count (63)
-    0x91,
-    0x83, //   Output (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Volatile)
-    0xC0, // End Collection
+    0x05, 0x01, // Usage Page (Generic Desktop)        0
+    0x15, 0x00, // Logical Minimum (0)                 2
+    0x09, 0x04, // Usage (Joystick)                    4
+    0xa1, 0x01, // Collection (Application)            6
+    0x85, 0x30, //  Report ID (48)                     8
+    0x05, 0x01, //  Usage Page (Generic Desktop)       10
+    0x05, 0x09, //  Usage Page (Button)                12
+    0x19, 0x01, //  Usage Minimum (1)                  14
+    0x29, 0x0a, //  Usage Maximum (10)                 16
+    0x15, 0x00, //  Logical Minimum (0)                18
+    0x25, 0x01, //  Logical Maximum (1)                20
+    0x75, 0x01, //  Report Size (1)                    22
+    0x95, 0x0a, //  Report Count (10)                  24
+    0x55, 0x00, //  Unit Exponent (0)                  26
+    0x65, 0x00, //  Unit (None)                        28
+    0x81, 0x02, //  Input (Data,Var,Abs)               30
+    0x05, 0x09, //  Usage Page (Button)                32
+    0x19, 0x0b, //  Usage Minimum (11)                 34
+    0x29, 0x0e, //  Usage Maximum (14)                 36
+    0x15, 0x00, //  Logical Minimum (0)                38
+    0x25, 0x01, //  Logical Maximum (1)                40
+    0x75, 0x01, //  Report Size (1)                    42
+    0x95, 0x04, //  Report Count (4)                   44
+    0x81, 0x02, //  Input (Data,Var,Abs)               46
+    0x75, 0x01, //  Report Size (1)                    48
+    0x95, 0x02, //  Report Count (2)                   50
+    0x81, 0x03, //  Input (Cnst,Var,Abs)               52
+    0x0b, 0x01, 0x00, 0x01, 0x00, //  Usage (Vendor Usage 0x10001)       54
+    0xa1, 0x00, //  Collection (Physical)              59
+    0x0b, 0x30, 0x00, 0x01, 0x00, //   Usage (Vendor Usage 0x10030)      61
+    0x0b, 0x31, 0x00, 0x01, 0x00, //   Usage (Vendor Usage 0x10031)      66
+    0x0b, 0x32, 0x00, 0x01, 0x00, //   Usage (Vendor Usage 0x10032)      71
+    0x0b, 0x35, 0x00, 0x01, 0x00, //   Usage (Vendor Usage 0x10035)      76
+    0x15, 0x00, //   Logical Minimum (0)               81
+    0x27, 0xff, 0xff, 0x00, 0x00, //   Logical Maximum (65535)           83
+    0x75, 0x10, //   Report Size (16)                  88
+    0x95, 0x04, //   Report Count (4)                  90
+    0x81, 0x02, //   Input (Data,Var,Abs)              92
+    0xc0, //  End Collection                     94
+    0x0b, 0x39, 0x00, 0x01, 0x00, //  Usage (Vendor Usage 0x10039)       95
+    0x15, 0x00, //  Logical Minimum (0)                100
+    0x25, 0x07, //  Logical Maximum (7)                102
+    0x35, 0x00, //  Physical Minimum (0)               104
+    0x46, 0x3b, 0x01, //  Physical Maximum (315)             106
+    0x65, 0x14, //  Unit (EnglishRotation: deg)        109
+    0x75, 0x04, //  Report Size (4)                    111
+    0x95, 0x01, //  Report Count (1)                   113
+    0x81, 0x02, //  Input (Data,Var,Abs)               115
+    0x05, 0x09, //  Usage Page (Button)                117
+    0x19, 0x0f, //  Usage Minimum (15)                 119
+    0x29, 0x12, //  Usage Maximum (18)                 121
+    0x15, 0x00, //  Logical Minimum (0)                123
+    0x25, 0x01, //  Logical Maximum (1)                125
+    0x75, 0x01, //  Report Size (1)                    127
+    0x95, 0x04, //  Report Count (4)                   129
+    0x81, 0x02, //  Input (Data,Var,Abs)               131
+    0x75, 0x08, //  Report Size (8)                    133
+    0x95, 0x34, //  Report Count (52)                  135
+    0x81, 0x03, //  Input (Cnst,Var,Abs)               137
+    0x06, 0x00, 0xff, //  Usage Page (Vendor Defined Page 1) 139
+    0x85, 0x21, //  Report ID (33)                     142
+    0x09, 0x01, //  Usage (Vendor Usage 1)             144
+    0x75, 0x08, //  Report Size (8)                    146
+    0x95, 0x3f, //  Report Count (63)                  148
+    0x81, 0x03, //  Input (Cnst,Var,Abs)               150
+    0x85, 0x81, //  Report ID (129)                    152
+    0x09, 0x02, //  Usage (Vendor Usage 2)             154
+    0x75, 0x08, //  Report Size (8)                    156
+    0x95, 0x3f, //  Report Count (63)                  158
+    0x81, 0x03, //  Input (Cnst,Var,Abs)               160
+    0x85, 0x01, //  Report ID (1)                      162
+    0x09, 0x03, //  Usage (Vendor Usage 0x03)          164
+    0x75, 0x08, //  Report Size (8)                    166
+    0x95, 0x3f, //  Report Count (63)                  168
+    0x91, 0x83, //  Output (Cnst,Var,Abs,Vol)          170
+    0x85, 0x10, //  Report ID (16)                     172
+    0x09, 0x04, //  Usage (Vendor Usage 0x04)          174
+    0x75, 0x08, //  Report Size (8)                    176
+    0x95, 0x3f, //  Report Count (63)                  178
+    0x91, 0x83, //  Output (Cnst,Var,Abs,Vol)          180
+    0x85, 0x80, //  Report ID (128)                    182
+    0x09, 0x05, //  Usage (Vendor Usage 0x05)          184
+    0x75, 0x08, //  Report Size (8)                    186
+    0x95, 0x3f, //  Report Count (63)                  188
+    0x91, 0x83, //  Output (Cnst,Var,Abs,Vol)          190
+    0x85, 0x82, //  Report ID (130)                    192
+    0x09, 0x06, //  Usage (Vendor Usage 0x06)          194
+    0x75, 0x08, //  Report Size (8)                    196
+    0x95, 0x3f, //  Report Count (63)                  198
+    0x91, 0x83, //  Output (Cnst,Var,Abs,Vol)          200
+    0xc0, // End Collection                      202
 ];
--- src/switch/hid_descriptor.rs
+++ src/switch/hid_descriptor.rs
@@ -2,29 +2,152 @@ use serde::ser::{Serialize, SerializeTuple, Serializer};
 use usbd_hid::descriptor::gen_hid_descriptor;
 use usbd_hid::descriptor::generator_prelude::*;
 
+#[derive(Copy, Clone, Debug)]
+pub enum Button {
+    A,
+    B,
+    X,
+    Y,
+    L,
+    R,
+    ZL,
+    ZR,
+    Plus,
+    Minus,
+    LeftStick,
+    RightStick,
+    Home,
+    Capture,
+}
+
+impl Button {
+    /// Returns the bit mask for the corresponding button.
+    pub fn as_value(self) -> (u8, u16) {
+        match self {
+            Button::A => (0, 1 << 0),
+            Button::B => (0, 1 << 1),
+            Button::X => (0, 1 << 2),
+            Button::Y => (0, 1 << 3),
+            Button::L => (0, 1 << 4),
+            Button::R => (0, 1 << 5),
+            Button::ZL => (0, 1 << 6),
+            Button::ZR => (0, 1 << 7),
+            Button::Plus => (0, 1 << 8),
+            Button::Minus => (0, 1 << 9),
+            Button::LeftStick => (1, 1 << 0),
+            Button::RightStick => (1, 1 << 1),
+            Button::Home => (1, 1 << 2),
+            Button::Capture => (1, 1 << 3),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum Dpad {
+    Neutral = 0x08, // No direction pressed
+    Up = 0x00,
+    UpRight = 0x01,
+    Right = 0x02,
+    DownRight = 0x03,
+    Down = 0x04,
+    DownLeft = 0x05,
+    Left = 0x06,
+    UpLeft = 0x07,
+}
+
+pub const JOYSTICK_CENTER: u16 = 32767;
+
 // source: <https://gist.github.com/ToadKing/b883a8ccfa26adcc6ba9905e75aeb4f2>
 #[gen_hid_descriptor(
-   (usage_page = GENERIC_DESKTOP, logical_min = 1, usage = JOYSTICK, collection = APPLICATION) = {
-      (usage_page = GENERIC_DESKTOP, usage_page = BUTTON, report_id = 0x30, usage_min = 0x01, usage_max = 0x0A, logical_min = 0, logical_max = 1, unit_exponent = 0) = {
-        #[data, variable, absolute, no_wrap, linear, preferred, not_null] ten_buttons=input;
-      };
-      (usage_page = BUTTON, usage_min = 0x0B, usage_max = 0x0E, logical_min = 0, logical_max = 1) = {
-        #[data, variable, absolute, no_wrap, linear, preferred, not_null] four_buttons=input;
-      };
-
-      // () = {
-      // #[data, variable, absolute, no_wrap, linear, preferred, not_null] four_buttons=input;
-      // }
-      // there is another one here?
-
-      (usage = 0x010001, collection = PHYSICAL, usage = 0x010030, usage = 0x010031, usage = 0x010032, usage = 0x010035, logical_min = 0, logical_max = 65534) = {
-        #[data, variable, absolute, no_wrap, linear, preferred, not_null] four_buttons=input;
-      };
+    (collection = APPLICATION, usage_page = GENERIC_DESKTOP, usage = JOYSTICK) = {
+        (usage_page = BUTTON, usage_min = 0x01, usage_max = 0x0A, logical_min = 0, logical_max = 1) = {
+            #[item_settings data, variable] buttons=input;
+        };
+        (usage_page = BUTTON, usage_min = 0x0B, usage_max = 0x0E, logical_min = 0, logical_max = 1) = {
+            #[item_settings data, variable] special_buttons=input;
+        };
+        (usage = 0x010030, logical_min = 0, logical_max = 65534 ) = {
+            #[item_settings data, variable] left_x=input;
+        };
+        (usage = 0x010031, logical_min = 0, logical_max = 65534) = {
+            #[item_settings data, variable] left_y=input;
+        };
+        (usage = 0x010032, logical_min = 0, logical_max = 65534) = {
+            #[item_settings data, variable] right_y=input;
+        };
+        (usage = 0x010035, logical_min = 0, logical_max = 65534) = {
+            #[item_settings data, variable] right_y=input;
+        };
+        (usage = 0x010039, logical_min = 0, logical_max = 7) = {
+            #[item_settings data, variable] d_pad=input;
+        };
     }
 )]
-pub struct ProControllerReport {
-    pub ten_buttons: u8,
-    pub four_buttons: u8,
-    pub x: i8,
-    pub y: i8,
+pub struct SwitchProControllerReport {
+    pub buttons: u16,        // First 10 buttons
+    pub special_buttons: u8, // Next 4 special buttons
+    pub left_x: u16,         // Left joystick X
+    pub left_y: u16,         // Left joystick Y
+    pub right_x: u16,        // Right joystick X
+    pub right_y: u16,        // Right joystick Y
+    pub d_pad: u8,           // Hat (D-Pad)
+}
+
+impl SwitchProControllerReport {
+    /// Creates a new, empty controller report.
+    pub fn new() -> Self {
+        Self {
+            buttons: 0,
+            special_buttons: 0,
+            left_x: JOYSTICK_CENTER, // Center position for 16-bit joystick values
+            left_y: JOYSTICK_CENTER,
+            right_x: JOYSTICK_CENTER,
+            right_y: JOYSTICK_CENTER,
+            d_pad: Dpad::Neutral as u8, // Neutral position for hat switch (8 means no direction pressed)
+        }
+    }
+
+    /// Press a named button.
+    pub fn press_button(&mut self, button: Button) {
+        let (group, mask) = button.as_value();
+        if group == 0 {
+            self.buttons |= mask;
+        } else {
+            self.special_buttons |= mask as u8;
+        }
+    }
+
+    /// Release a named button.
+    pub fn release_button(&mut self, button: Button) {
+        let (group, mask) = button.as_value();
+        if group == 0 {
+            self.buttons &= !mask;
+        } else {
+            self.special_buttons &= !(mask as u8);
+        }
+    }
+
+    /// Set the hat switch to a named direction.
+    pub fn set_dpad(&mut self, position: Dpad) {
+        self.d_pad = position as u8;
+    }
+
+    /// Set the joystick positions (left stick: x, y; right stick: z, rz).
+    pub fn set_joystick(&mut self, lx: u16, ly: u16, rx: u16, ry: u16) {
+        self.left_x = lx;
+        self.left_y = ly;
+        self.right_x = rx;
+        self.right_y = ry;
+    }
+
+    /// Convenience method to reset all buttons and set joysticks to neutral position.
+    pub fn reset(&mut self) {
+        self.buttons = 0;
+        self.special_buttons = 0;
+        self.left_x = JOYSTICK_CENTER;
+        self.left_y = JOYSTICK_CENTER;
+        self.right_x = JOYSTICK_CENTER;
+        self.right_y = JOYSTICK_CENTER;
+        self.d_pad = Dpad::Neutral as u8;
+    }
 }
